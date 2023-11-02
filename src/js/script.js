7cfc30f20c859c31d7593ef2d1a69dc118eba77e
iori <09058798413iori@gmail.com>
2023-11-03 05:56:09 +0900

    rem

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,40 @@
 
+"use strict";
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+  
+/* 表　スクロールの案内
+***************************************************************/
+  new ScrollHint('.js-table', {
+    suggestiveShadow: true,
+    remainingTime: 5000,
+    i18n: {
+      scrollable: 'スクロール'
+    },
+  });
+
+
+/* ふわっと表示
+***************************************************************/
+  $(window).on("scroll", function () {
+    let e = $(window).scrollTop() + $(window).height();
+    $(".fadeup").each(function () {
+      var t = $(this).offset().top;
+      e > t + 100 && $(this).addClass("show");
+    });
+  })
+  
+  function load_effect() {
+    var element = document.getElementsByClassName('load-fade');
+    if(!element) return; // 要素がない場合は終了
+    
+    for(var i = 0; i < element.length; i++) { 
+      element[i].classList.add('is-show');
+    }
+  }
+  setTimeout(load_effect, 100); // 100ミリ秒経過後に実行
+
+  
 
 });
+
